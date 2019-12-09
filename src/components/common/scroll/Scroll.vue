7cfb37527cf750e68a5d095fee58565cc20f04f9
AntoineYu <yzq356350@gmail.com>
2019-12-09 14:28:22 +0100

    BScroll的封装

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,27 @@
+<template>
+  <div calss="wrapper" ref="wrapper">
+      <div class="content">
+          <slot>
+          </slot>
+      </div>
+  </div>
+</template>
+<script>
+import BScroll from 'better-scroll'
+export default {
+    name: 'scroll',
+    data() {
+        return {
+            scroll: null
+        }
+    },
+    mounted() {
+        this.scroll = new BScroll(this.$refs.wrapper, {
+            click: true
+        })
+    }
+}
+</script>
+<style scoped>
+
+</style>

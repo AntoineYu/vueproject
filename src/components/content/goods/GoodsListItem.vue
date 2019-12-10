6b672ebe98f2a8595b2fa713f217b4795e8a0246
AntoineYu <yzq356350@gmail.com>
2019-12-10 17:25:28 +0100

    跳转到详情页并传id

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="goods-item">
+  <div class="goods-item" @click="itemClick">
       <img src="goodsItem.show.img" alt="" @load="imgeLoad">
       <div class="goods-info">
           <p>{{ goodsItem.title }}</p>
@@ -23,6 +23,9 @@ export default {
       imgeLoad() {
         //事件总线
         this.$bus.$emit('itemImgLoad')
+      },
+      itemClick() {
+        this.$router.push('/detail' + this.goodsItem.iid)
       }
     }
 }
